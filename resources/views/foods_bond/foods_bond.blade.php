e5b26d5d5dee9b399c9f7a4a912022b9a7729c33
nozaki59 <107387074+nozaki59@users.noreply.github.com>
2024-05-01 17:41:07 +0900

    foods bond 5月 fix

--- resources/views/foods_bond/foods_bond.blade.php
+++ resources/views/foods_bond/foods_bond.blade.php
@@ -275,7 +275,7 @@
                     </div>
                 </div>
             </div>
-            <div id="May" class="flex flex-col w-full justify-center items-center bg-white rounded-lg border border-blue-900">
+            <div id="May" class="hidden flex flex-col w-full justify-center items-center bg-white rounded-lg border border-blue-900">
                 <p class="text-6xl font-bold ja-display-font">May</p>
                 <div class="flex flex-col md:flex-row w-full justify-evenly items-center p-0 md:p-4">
                     <div class="flex flex-col md:flex-row justify-center items-center max-w-full md:max-w-[50%] p-4 gap-4">
