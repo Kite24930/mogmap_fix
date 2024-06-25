4ba56006df3bec24f1c044e333adb9a0d614e428
nozaki59 <107387074+nozaki59@users.noreply.github.com>
2024-06-25 09:33:26 +0900

    foods bond 7月

--- resources/views/foods_bond/foods_bond.blade.php
+++ resources/views/foods_bond/foods_bond.blade.php
@@ -353,6 +353,45 @@
                     </div>
                 </div>
             </div>
+            <div id="July" class="flex flex-col w-full justify-center items-center bg-white rounded-lg border border-blue-900">
+                <p class="text-6xl font-bold ja-display-font">July</p>
+                <div class="flex flex-col md:flex-row w-full justify-evenly items-center p-0 md:p-4">
+                    <div class="flex flex-col md:flex-row justify-center items-center max-w-full md:max-w-[50%] p-4 gap-4">
+                        <div class="edu-center flex flex-col items-center p-4 bg-pink-200 border border-pink-400 rounded-lg w-full">
+                            <p class="text-xl font-bold mb-2">教職支援センター前</p>
+                            <div class="swiper flipSwiper max-w-full md:max-w-[350px]">
+                                <div class="swiper-wrapper w-full">
+                                    <div class="swiper-slide p-4 bg-white rounded-lg border border-blue-900">
+                                        <x-foods_bond.visiter-card shopName="Nageia" shopImg="nageia.jpeg" instagram="nageia_trailer" targetDate="7/9(Tue)" appeal="モンブランソフト" />
+                                    </div>
+                                    <div class="swiper-slide p-4 bg-white rounded-lg border border-blue-900">
+                                        <x-foods_bond.visiter-card shopName="クッピーラムネカフェ" shopImg="kuppyramune.jpeg" instagram="kuppyramune.cafe" targetDate="7/11(Thu)" appeal="クッピーラムネ🥤" />
+                                    </div>
+                                </div>
+                                <div class="swiper-button-next"></div>
+                                <div class="swiper-button-prev"></div>
+                                <div class="swiper-pagination"></div>
+                            </div>
+                        </div>
+                        <div class="sansui-hole flex flex-col items-center w-full p-4 bg-green-200 border border-green-400 rounded-lg">
+                            <p class="text-2xl font-bold mb-2">三翠ホール前</p>
+                            <div class="swiper flipSwiper max-w-full md:max-w-[350px]">
+                                <div class="swiper-wrapper w-full">
+                                    <div class="swiper-slide p-4 bg-white rounded-lg border border-blue-900">
+                                        <x-foods_bond.visiter-card shopName="Lbe" shopImg="Lbe.jpg" instagram="lbe_397" targetDate="7/9(Tue)" appeal="ホットドッグ🌭" />
+                                    </div>
+                                    <div class="swiper-slide p-4 bg-white rounded-lg border border-blue-900">
+                                        <x-foods_bond.shop-card :data="$shop_lists->find(17)" targetDate="7/11(Thu)" appeal="きゅうりタコス🥒" />
+                                    </div>
+                                </div>
+                                <div class="swiper-button-next"></div>
+                                <div class="swiper-button-prev"></div>
+                                <div class="swiper-pagination"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
     @vite(['resources/js/foods_bond.js'])
