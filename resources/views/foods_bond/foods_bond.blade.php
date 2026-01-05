c84d6a4a262c6a82d288cca56ca2b4d1c886054a
Kite24930 <nt930.kt311@gmail.com>
2026-01-05 13:19:23 +0900

    foods bond 1月 add fix

--- resources/views/foods_bond/foods_bond.blade.php
+++ resources/views/foods_bond/foods_bond.blade.php
@@ -797,7 +797,7 @@
                     </div>
                 </div>
             </div>
-            <div id="December" class="flex flex-col w-full justify-center items-center bg-white rounded-lg border border-blue-900">
+            <div id="December" class="hidden flex-col w-full justify-center items-center bg-white rounded-lg border border-blue-900">
                 <p class="text-6xl font-bold ja-display-font">December</p>
                 <div class="flex flex-col md:flex-row w-full justify-evenly items-center p-0 md:p-4">
                     <div class="flex flex-col md:flex-row justify-center items-center max-w-full md:max-w-[50%] p-4 gap-4">
@@ -830,7 +830,7 @@
                     </div>
                 </div>
             </div>
-            <div id="January" class="hidden flex-col w-full justify-center items-center bg-white rounded-lg border border-blue-900">
+            <div id="January" class="flex flex-col w-full justify-center items-center bg-white rounded-lg border border-blue-900">
                 <p class="text-6xl font-bold ja-display-font">January</p>
                 <div class="flex flex-col md:flex-row w-full justify-evenly items-center p-0 md:p-4">
                     <div class="flex flex-col md:flex-row justify-center items-center max-w-full md:max-w-[50%] p-4 gap-4">
